eba3ad3374db3dc170c3b8b306b48026c313c71b
Prince0909070 <151009341+Prince0909070@users.noreply.github.com>
2023-12-20 23:11:11 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main(){
+    int PI;
+    int a=1;
+    for(int i=1;i<=20000;i++){
+        PI+=400000/((2*i-1)*a);
+        printf("%d\n",PI);
+        a*=-1;
+        if(PI==314159){
+            printf("%d",2*i-1);
+            break;
+        }
+    }
+    
+    return 0;
+}

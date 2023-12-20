8cb58aa71724b74f326adcf8286a971679a1df7b
Prince0909070 <151009341+Prince0909070@users.noreply.github.com>
2023-12-20 22:59:57 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-int main() {
-  int n=7
-  for(
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
+int main(){
+    int i,j;
+    for(i=1;i<=7;i++){
+        printf("%*d",8-i,i);
+        for(j=1;j<=i-1;j++){
+            printf("%*d",2,i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
 }
